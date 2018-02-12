8e0b146969b0aa7bce3e9d1257c23fd8590ee0f9
Takafumi Horiuchi <hawkletter080e@icloud.com>
2018-02-12 12:46:18 +0900

    basic moves

--- brainstorm.py
+++ brainstorm.py
@@ -3,33 +3,44 @@ import pygame
 from pygame.locals import *  # constants such as QUIT
 import sys
 
-SCREEN_SIZE = (1200, 800)
+SCREEN_W = 1200
+SCREEN_H = 800
+SCREEN_SIZE = (SCREEN_W, SCREEN_H)
 
 pygame.init()
 # may create only one screen with set_mode()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("balloonfight")
 
+x = 640
+y = 420
+vx = 20
+vy = 20
 img = pygame.image.load("./images/img_fly.gif").convert_alpha()
 img_rect = img.get_rect()
-img_rect.center = (640, 420)
+img_rect.center = (x, y)
 
-vx = vy = 20
+img_enemy = pygame.image.load("./images/mario.gif").convert_alpha()
 
 # game loop
 while True:
     pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[K_LEFT] && :
+    if pressed_keys[K_LEFT] and (x > 0):
         img_rect.move_ip(-vx, 0)
-    if pressed_keys[K_RIGHT]:
+        x -= vx
+    if pressed_keys[K_RIGHT] and (x < SCREEN_W):
         img_rect.move_ip(vx, 0)
-    if pressed_keys[K_UP]:
+        x += vx
+    if pressed_keys[K_UP] and (y > 0):
         img_rect.move_ip(0, -vy)
-    if pressed_keys[K_DOWN]:
+        y -= vy
+    if pressed_keys[K_DOWN] and (y < SCREEN_H):
         img_rect.move_ip(0, vy)
+        y += vy
 
     screen.fill((0,0,0))  # screen color
     
+    screen.blit(img_enemy, (SCREEN_W/2, SCREEN_H/2))
     screen.blit(img, img_rect)
     
     pygame.display.update()  # update screen
